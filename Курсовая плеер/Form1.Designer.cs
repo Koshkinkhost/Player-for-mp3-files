a41c23c3667b26209798334f4df68a6221bb1ac8
boris away <boris away@DESKTOP-QK0CGSD>
2023-01-12 13:01:09 +0300

    Добавьте файлы проекта.

---
+++ Курсовая плеер/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Курсовая_плеер
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.buttonplay = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Плейлист = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // buttonplay
+            // 
+            this.buttonplay.BackColor = System.Drawing.Color.DarkKhaki;
+            this.buttonplay.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonplay.Font = new System.Drawing.Font("Lucida Sans", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonplay.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.buttonplay.Location = new System.Drawing.Point(159, 255);
+            this.buttonplay.Name = "buttonplay";
+            this.buttonplay.Size = new System.Drawing.Size(110, 40);
+            this.buttonplay.TabIndex = 1;
+            this.buttonplay.Text = "PLAY";
+            this.buttonplay.UseVisualStyleBackColor = false;
+            this.buttonplay.Click += new System.EventHandler(this.button2_Click);
+            this.buttonplay.MouseDown += new System.Windows.Forms.MouseEventHandler(this.buttonplay_MouseDown);
+            this.buttonplay.MouseEnter += new System.EventHandler(this.button2_MouseEnter);
+            this.buttonplay.MouseUp += new System.Windows.Forms.MouseEventHandler(this.buttonplay_MouseUp);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkKhaki;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Lucida Sans", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(623, 255);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 40);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Pause";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            this.button1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button1_MouseDown);
+            this.button1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.button1_MouseUp);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.textBox1.Location = new System.Drawing.Point(0, 184);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(882, 61);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Плейлист
+            // 
+            this.Плейлист.BackColor = System.Drawing.Color.LemonChiffon;
+            this.Плейлист.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Плейлист.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Плейлист.ForeColor = System.Drawing.Color.DarkGoldenrod;
+            this.Плейлист.FormattingEnabled = true;
+            this.Плейлист.ItemHeight = 16;
+            this.Плейлист.Location = new System.Drawing.Point(0, 0);
+            this.Плейлист.Name = "Плейлист";
+            this.Плейлист.Size = new System.Drawing.Size(882, 178);
+            this.Плейлист.TabIndex = 6;
+            this.Плейлист.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DarkKhaki;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Lucida Sans", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Location = new System.Drawing.Point(507, 255);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(110, 40);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "next";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            this.button2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button2_MouseDown);
+            this.button2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.button2_MouseUp);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.DarkKhaki;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Location = new System.Drawing.Point(391, 255);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(110, 40);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Open";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button3_MouseDown);
+            this.button3.MouseUp += new System.Windows.Forms.MouseEventHandler(this.button3_MouseUp);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.DarkKhaki;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button4.Font = new System.Drawing.Font("Lucida Sans", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(275, 255);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(110, 40);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "previous";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.button4.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button4_MouseDown);
+            this.button4.MouseUp += new System.Windows.Forms.MouseEventHandler(this.button4_MouseUp);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(882, 453);
+            this.Controls.Add(this.Плейлист);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonplay);
+            this.Name = "Form1";
+            this.Text = "Player";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonplay;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox Плейлист;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        public System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Курсовая плеер/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+using System.Windows.Forms;
+using Microsoft.DirectX.AudioVideoPlayback;
+using WMPLib;
+namespace Курсовая_плеер
+{
+    public partial class Form1 : Form
+    {
+        
+        private Audio audio;
+        public string[] songs;
+        public string[] paths;
+        private string folderpath = @"C:\Users\boris\Music\iTunes\";
+        string s=string.Empty;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static int PlayPause = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        WindowsMediaPlayer WMP = new WMPLib.WindowsMediaPlayer();
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //progressBar1.PerformStep(); 
+            
+
+                this.Text = WMP.versionInfo;
+            
+            
+            WMP.controls.play();
+            
+            
+            
+            
+
+
+        }
+
+        private void button2_MouseEnter(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void toolStripDropDownButton1_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+        OpenFileDialog o = new OpenFileDialog()
+        {
+            Filter = "MP3|*.mp3"
+        };
+        private void openFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            if (o.ShowDialog() == DialogResult.OK)
+                s = o.FileName;
+            
+              Плейлист.Items.Add(s);
+
+        }
+      
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            
+            WMP.controls.pause();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+                WMP.URL = paths[Плейлист.SelectedIndex];
+                WMP.controls.play();
+                textBox1.Text = $"Сейчас играет{paths[Плейлист.SelectedIndex]}";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+                OpenFileDialog o = new OpenFileDialog();
+            o.Filter = "MP3|*.mp3";
+            o.Multiselect = true;
+            if (o.ShowDialog() ==System.Windows.Forms.DialogResult.OK)
+            {
+                songs = o.FileNames;
+                paths = o.FileNames;
+                for (int i = 0; i < songs.Length; i++)
+                {
+                    Плейлист.Items.Add(songs[i]);
+                }
+            }
+            
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            if (Плейлист.SelectedIndex < Плейлист.Items.Count-1)
+                Плейлист.SelectedIndex = Плейлист.SelectedIndex + 1;
+            else
+                MessageBox.Show("Конец плейлиста");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if(Плейлист.SelectedIndex>0)
+                Плейлист.SelectedIndex = Плейлист.SelectedIndex - 1;
+            else
+            {
+                MessageBox.Show("Вы в самом начале плейлиста");
+            }
+        }
+
+        private void buttonplay_MouseDown(object sender, MouseEventArgs e)
+        {
+            buttonplay.BackColor = Color.Khaki;
+        }
+
+        private void buttonplay_MouseUp(object sender, MouseEventArgs e)
+        {
+            button4.BackColor = Color.DarkKhaki;
+        }
+
+        private void button4_MouseDown(object sender, MouseEventArgs e)
+        {
+            button4.BackColor = Color.Khaki;
+        }
+
+        private void button4_MouseUp(object sender, MouseEventArgs e)
+        {
+            button4.BackColor = Color.DarkKhaki;
+        }
+
+        private void button3_MouseDown(object sender, MouseEventArgs e)
+        {
+            button3.BackColor = Color.Khaki;
+        }
+
+        private void button3_MouseUp(object sender, MouseEventArgs e)
+        {
+            button3.BackColor = Color.DarkKhaki;
+        }
+
+        private void button2_MouseDown(object sender, MouseEventArgs e)
+        {
+            button2.BackColor = Color.Khaki;
+        }
+
+        private void button2_MouseUp(object sender, MouseEventArgs e)
+        {
+            button2.BackColor = Color.DarkKhaki;
+        }
+
+        private void button1_MouseDown(object sender, MouseEventArgs e)
+        {
+            button1.BackColor = Color.Khaki;
+        }
+
+        private void button1_MouseUp(object sender, MouseEventArgs e)
+        {
+            button1.BackColor = Color.DarkKhaki;
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+        }
+    }
+            
+}
+
